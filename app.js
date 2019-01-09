61b022a89affcc1d0eb19d0075df0e125439cf35
Your Name <you@example.com>
2019-01-09 22:07:39 +0000

    fixing port number

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var port = port = process.env.PORT || 80;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(8080);
+}).listen(port);
 console.log('Server running at http://localhost:8080/');
