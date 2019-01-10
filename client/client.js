9b5c3036d64927e7b1110d36853aa2870dde8457
Your Name <you@example.com>
2019-01-10 23:12:20 +0000

    adding socket.io lib fix

--- client/client.js
+++ client/client.js
@@ -5,6 +5,27 @@ socket.on('connect', function(){
 
 socket.on('build', function(data){
   console.log('build ...')
+
+  const https = require('https');
+
+  https.get('https://google.com/', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+      console.log(data);
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(JSON.parse(data).explanation);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
 });
 
 socket.on('disconnect', function(){
